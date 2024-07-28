54fefc3559b9f87f440606d991f4477e531ecf6f
RafaelBlackwood <rafael.agashirinov@gmail.com>
2024-07-28 20:32:41 +0200

    finished implementation with java calculator

--- java/Calculator/src/Calculator.java
+++ java/Calculator/src/Calculator.java
@@ -21,21 +21,60 @@ public class Calculator {
         }
 
     private char[] parseOperators(String expression) {
+        return expression.replaceAll("[^+*/-]", "").toCharArray();
     }
 
     private double[] parseNumbers(String expression) {
+        // Find all numbers in the expression
+        String[] parts = expression.split("[+*/-]");
+        double[] numbers = new double[parts.length];
+
+        for (int i = 0; i < parts.length; i++) {
+            numbers[i] = Double.parseDouble(parts[i]);
+        }
+        return numbers;
     }
 
+    // Apply operator precedence
     private double applyOperatorPrecedence(double[] numbers, char[] operators) {
-        for (int i = 0; i < operators.length; i++) {
+        int numberCount = numbers.length;
+        int operatorCount = operators.length;
+
+        // Сначала обрабатываем умножение и деление
+        for (int i = 0; i < operatorCount; i++) {
             if (operators[i] == '*' || operators[i] == '/') {
                 if (operators[i] == '*') {
                     numbers[i] = numbers[i] * numbers[i + 1];
+                } else if (operators[i] == '/') {
+                    numbers[i] = numbers[i] / numbers[i + 1];
                 }
-                else if (operators[i] == '/') {
-                numbers[i] = numbers[i] / numbers[i + 1];
+
+                // Сдвиг оставшихся чисел и операторов
+                for (int j = i + 1; j < numberCount - 1; j++) {
+                    numbers[j] = numbers[j + 1];
                 }
+                numberCount--;
+
+                for (int j = i; j < operatorCount - 1; j++) {
+                    operators[j] = operators[j + 1];
+                }
+                operatorCount--;
+
+                i--; // Перепроверка оператора на текущем индексе
             }
+        }
+
+        // Обрабатываем сложение и вычитание
+        double result = numbers[0];
+        for (int i = 0; i < operatorCount; i++) {
+            if (operators[i] == '+') {
+                result += numbers[i + 1];
+            } else if (operators[i] == '-') {
+                result -= numbers[i + 1];
+            }
+        }
+
+        return result;
     }
 
 
--- java/Calculator/src/Main.java
+++ java/Calculator/src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
         System.out.println("Enter a mathematical expression:");
         String expression = sc.nextLine();
+
+        System.out.println(calc.calculate(expression));
     }
 
 }
